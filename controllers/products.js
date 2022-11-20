1c6a4152b77a97674d074b6f2fbc18784cc5098b
Awosise Oluwaseun <awosiseo@gmail.com>
2022-11-20 09:50:19 +0100

    update: query parameters

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,42 @@
-const getAllProducts = async (req, res, next) => {}
+const Product = require("../models/products")
+
+const getAllProducts = async (req, res, next) => {
+    const { featured, company, name, sort, fields } = req.query
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+
+    if (company) {
+        queryObject.company = company
+    }
+
+    if (name) {
+        queryObject.name = { $regex: name, $options: "i" }
+    }
+    let result = Product.find(queryObject)
+
+    if (sort) {
+        const sortList = sort.split(",").join(" ")
+        result = result.sort(sortList)
+    }else {
+        result = result.sort("createdAt")
+    }
+
+    if (fields) {
+        const fieldsList = fields.split(",").join(" ")
+        result = result.select(fieldsList)
+    }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+
+    result = result.skip(skip).limit(limit)
+    const products = await result
+    res.status(200).json({ products, nbHits: products.length })
+}
 
 const getAllProductsStatic = async (req, res, next) => {}
 
