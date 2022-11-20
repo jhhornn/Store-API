931a5409cb1060461434c8c66f5a1098d2520001
Awosise Oluwaseun <awosiseo@gmail.com>
2022-11-20 15:31:15 +0100

    update: removed test route

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,8 @@
 const express = require("express")
 const productsRouter = express.Router()
 
-const {
-  getAllProducts,
-  getAllProductsStatic
-} = require("../controllers/products")
+const getAllProducts = require("../controllers/products")
 
 productsRouter.route("/").get(getAllProducts)
-
-productsRouter.route("/static").get(getAllProductsStatic)
 
 module.exports = productsRouter
