59fe5cbd45acf4383db0d34a878cc9d1ae6e678d
Awosise Oluwaseun <awosiseo@gmail.com>
2022-11-20 15:33:12 +0100

    Prettified

--- populate.js
+++ populate.js
@@ -4,16 +4,16 @@ const connectDB = require("./config/database")
 const jsonProduct = require("./products.json")
 
 const start = async () => {
-    try {
-        await connectDB()
-        await Product.deleteMany()
-        await Product.create(jsonProduct)
-        console.log("Success!!!")
-        process.exit(0)
-    } catch (err) {
-        console.log(err)
-        process.exit(1)
-    }
+  try {
+    await connectDB()
+    await Product.deleteMany()
+    await Product.create(jsonProduct)
+    console.log("Success!!!")
+    process.exit(0)
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
 }
 
 start()
