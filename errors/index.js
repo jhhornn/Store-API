d7acef9b26e6e5bb5b48b9b8d6c844fde1d6e3a0
Awosise Oluwaseun <awosiseo@gmail.com>
2022-11-25 15:41:44 +0100

    feat: index module to export all other error modules

---
+++ errors/index.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require("./custom-error")
+const Unauthenticated = require("./unauthenticated")
+const BadRequest = require("./bad-request")
+
+module.exports = {
+    CustomAPIError,
+    Unauthenticated,
+    BadRequest
+}
